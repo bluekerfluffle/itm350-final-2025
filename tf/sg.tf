598599102c5b18b75d428983d5c359a4413cdd4e
bluekerfluffle <157253515+bluekerfluffle@users.noreply.github.com>
2025-04-10 14:00:43 -0600

    Update sg.tf

--- tf/sg.tf
+++ tf/sg.tf
@@ -17,3 +17,23 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_security_group" "ecs_lb_sg" {
+  name_prefix = "ecs-lb-sg"
+  description = "Allow inbound HTTP from anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
