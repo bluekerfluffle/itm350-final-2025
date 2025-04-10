93f636aef2e75e83b8c34568f2dc977456bd607f
bluekerfluffle <157253515+bluekerfluffle@users.noreply.github.com>
2025-04-10 13:48:23 -0600

    Update vpc.tf

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -1,6 +1,17 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "main-vpc" }
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -9,7 +20,9 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
-  tags = { Name = "public-subnet-${count.index}" }
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
 }
 
 data "aws_availability_zones" "available" {}
