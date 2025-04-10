d05827a261b2ebef0718a01114281d12f62f04d5
bluekerfluffle <157253515+bluekerfluffle@users.noreply.github.com>
2025-04-10 14:13:02 -0600

    Update vpc.tf

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -26,3 +26,19 @@ resource "aws_subnet" "public" {
 }
 
 data "aws_availability_zones" "available" {}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_association" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
